1dd1aa372cc11f927be29adb46ac7d7d615518cd
BobTheFarmer <colinmweis@gmail.com>
2023-02-06 13:27:30 -0800

    Worked on frontend of fd

--- famousdestinations.js
+++ famousdestinations.js
@@ -1,3 +1,4 @@
+/* Temporary sorting testing*/
 const ascValButton = document.getElementById('asc')
 const decValButton = document.getElementById('dec')
 const ascLikeButton = document.getElementById('ascName')
@@ -19,11 +20,65 @@ decLikeButton.addEventListener('click', () => {
 
 
 const order = function() {
-  const ordered = [...document.getElementsByClassName('col')].sort(currentOrder)
+  const ordered = [...document.getElementsByClassName('container')].sort(currentOrder)
   ordered.forEach((elem, index) => {
     elem.style.order = index
   })
 }
 
-
 order()
+
+/* Post*/
+$(document).ready(function() {
+  $("#post-form").submit(function(e) {
+    e.preventDefault();
+    var title = $("#post-title").val();
+    var text = $("#post-text").val();
+    var likes = 0;
+    var postHTML = 
+      '<div class="container">' +
+        '<div class="image-column">' +
+          '<div class="img-cropped"></div>' +
+          '<div class="likes-row">' +
+            '<div class="like-button"></div>' +
+            '<div class="dislike-button"></div>' +
+            '<div class="text-likes">' + likes + ' Likes</div>' +
+          '</div>' +
+        '</div>' +
+        '<div class="text-column">' +
+          '<div class="text-subtitle">' + title + '</div>' +
+          '<div class="text-description">' + text + '</div>' +
+        '</div>' +
+      '</div>';
+    $("#posts").append(postHTML);
+    $(".like-button").last().click(function() {
+      likes++;
+      $(this).siblings(".text-likes").text(likes + " Likes");
+    });
+    $(".dislike-button").last().click(function() {
+      likes--;
+      $(this).siblings(".text-likes").text(likes + " Likes");
+    });
+
+    $.ajax({
+      url: "https://api.openai.com/v1/images/generations",
+      type: "post",
+      headers: {
+        "Authorization": "Bearer KEYGOESHERE"
+      },
+      data: JSON.stringify({
+        "model": "image-alpha-001",
+        "prompt": title,
+        "num_images":1,
+        "size":"1024x1024"
+      }),
+      contentType: "application/json; charset=utf-8",
+      dataType: "json",
+      success: function(data) {
+        window.alert("test")
+        $(".img-cropped").last().css("background-image", "url(" + data.data[0].url + ")");
+      }
+    });
+  });
+});
+
