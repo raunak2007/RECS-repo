a3603152df9b1a263a7b815fb3bc5e5581400738
EdwinKuttappi <edwinab0007@gmail.com>
2023-02-28 01:59:34 -0800

    coordinates3

--- maps.js
+++ maps.js
@@ -7,7 +7,7 @@ const create_fetch = apiUrl + '/create';
 const read_fetch = apiUrl + '/';
 const delete_fetch = apiUrl + '/delete';
 
-var map = L.map('map').setView([20.5, 10], 10);
+var map = L.map('map').setView([40.5, -0.1], 12);
 L.tileLayer('https://maptiles.p.rapidapi.com/en/map/v1/{z}/{x}/{y}.png?rapidapi-key=8d786c8035msh5ba0e19d2bd688ap10b377jsnd845f2afcfac', {
 attribution: 'Tiles &copy: <a href="https://www.maptilesapi.com/">MapTiles API</a>, Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 maxZoom: 19
