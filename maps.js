b3f6b6df1764b1bcb95a724509c7e6bc35968910
EdwinKuttappi <edwinab0007@gmail.com>
2023-02-27 21:13:52 -0800

    maps

--- maps.js
+++ maps.js
@@ -7,96 +7,235 @@ const create_fetch = apiUrl + '/create';
 const read_fetch = apiUrl + '/';
 const delete_fetch = apiUrl + '/delete';
 
+// Load cities and corresponding airports on page entry fetched from backend database
+read_entries();
 
-$(document).ready(function() {
-  getPosts();
-  function addPost(post) {
-    var likes = post.likes;
-    var postHTML = 
-      '<div class="container" id="post-' + post.id + '">' + /*
-        '<div class="image-column">' +
-          '<div class="img-cropped" id="img-post-' + post.id + '"></div>' +
-          '<div class="likes-row">' +
-            '<div class="like-button" data-liked="' + post.liked + '"></div>' +
-            '<div class="dislike-button"></div>' +
-            '<div class="text-likes">' + likes + ' Likes</div>' +
-            '<div class="report-button"></div>' +
-          '</div>' +
-        '</div>' + */
-        '<div class="text-column">' +
-          '<div class="text-subtitle">' + post.title + '</div>' +
-          '<div class="text-description">' + post.text + '</div>' +
-        '</div>' +
-      '</div>';
-    $("#posts").append(postHTML);
-    /*$(".like-button").last().click(function() {
-      likes++;
-      $(this).siblings(".text-likes").text(likes + " Likes");
-    });
-    $(".dislike-button").last().click(function() {
-      likes--;
-      $(this).siblings(".text-likes").text(likes + " Likes");
-    });*/
-  }
+// JavaScript Code to initialize the map
+var map = L.map('map').setView([51.5, -0.1], 12);
+L.tileLayer('https://maptiles.p.rapidapi.com/en/map/v1/3/4/2.png?rapidapi-key=8d786c8035msh5ba0e19d2bd688ap10b377jsnd845f2afcfac', {
+attribution: 'Tiles &copy: <a href="https://www.maptilesapi.com/">MapTiles API</a>, Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+maxZoom: 19
+}).addTo(map);
 
-  function getPosts() {
-    var url = "http://127.0.0.1:8012/api/fd";
-    let request = new XMLHttpRequest();
-    request.open("GET", url);
-    request.send();
-    request.onload = () => { 
-      if (request.status == 200) {
-        let posts = JSON.parse(request.response);
-        Object.entries(posts).forEach((post) => {
-          addPost(post[1]);
-        });
-      } else {
-        window.alert("ERROR: Failed to pull posts from API - Try refreshing or check for firewall");
-      }
-    };
-  }
-  $("#post-form").submit(function(e) {
-    e.preventDefault();
-    sendPost();
-  });
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '8d786c8035msh5ba0e19d2bd688ap10b377jsnd845f2afcfac',
+		'X-RapidAPI-Host': 'maptiles.p.rapidapi.com'
+	}
+};
 
-  function sendPost() {
-    var title = $("#post-title").val();
-    var text = $("#post-text").val();
-  
-    //Get image
-    /*fetch('https://api.openai.com/v1/images/generations', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer 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'
-      },
-      body: JSON.stringify({
-        "model": "image-alpha-001",
-        "prompt": title,
-        "num_images": 1,
-        "size": "1024x1024",
-        "response_format": "url"
-      })
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        // Save the image URL from the API response to the imageURL variable
-        var imageURL = data.data[0].url;*/
 
-        // Send all data
-  var url = "http://127.0.0.1:8012/api/fd/post";
-  fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      title: title,
-      text: text,
-      imageURL: "temp"
-    })
-     })
-    .then((response) => response.json())
+
+var dictionary = 0
+
+for (let i = 0; i < response.length; i++){
+    if (response[i]['name'] == cityField){
+        dictionary = response[i]
+        console.log(dictionary) 
     }
-});
+}
+
+if (dictionary == 0){
+    document.getElementById("resultText").remove();
+    const textDiv = document.getElementById('text');    
+    const p = document.createElement('P');
+    const pText = document.createTextNode("City not found.");
+    p.id = "resultText";
+    textDiv.appendChild(p);
+    p.appendChild(pText);
+    error = 1;
+}
+
+// Code to search for the user input city in the API response and fetch latitude and longitude for it
+
+        latitude = dictionary.coordinates.lat
+        longitude = dictionary.coordinates.lon
+
+        url = 'https://aerodatabox.p.rapidapi.com/airports/search/location/' + latitude + '/' + longitude + '/km/50/16?withFlightInfoOnly=false'
+
+        document.getElementById("resultText").remove()
+
+        const textDiv = document.getElementById('text');
+        
+        const p = document.createElement('P');
+        const pText = document.createTextNode('');
+
+        textDiv.appendChild(p);
+        p.appendChild(pText);
+        p.id = "resultText"
+
+        const aerodataboxOptions = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': 'f56b20ef1cmsh82a127be1b400c6p1e21bcjsn093efcf02b8f',
+                'X-RapidAPI-Host': 'aerodatabox.p.rapidapi.com'
+            }
+        };
+
+    fetch(url, aerodataboxOptions)
+        .then(response => response.json())
+        .then(response => {
+            console.log(response)
+
+airport = response.items[0].name
+var num = 0
+
+// for (let i = 0; i < airport.length; i++){
+//     if (airport[i] == ","){
+//         num += 2
+//         break
+//     }
+//     num += 1
+// }
+// airport = airport.slice(num)
+
+// Code to extract airport name and print on the webpage
+            if (error == 0){
+                const textDiv = document.getElementById('text');
+            
+                // const p = document.createElement('P');
+                const pText = document.createTextNode("Nearest Airport to " + cityField + ": " + airport);
+
+                // textDiv.appendChild(p);
+                p.appendChild(pText);
+                create_entry(cityField, airport);
+            }
+            else
+            {
+              error = 0;
+            }
+            
+            })
+        .catch(err => console.error(err));
+        })
+	.catch(err => console.error(err));
+}
+
+var subButton = document.getElementById('subButton');
+subButton.addEventListener('click', getAirportName, false); 
+
+
+// Function to post an entry into the database
+
+function create_entry(cityName, airportName){
+  const body = {
+      city: cityName,
+      airport: airportName
+  };
+  const requestOptions = {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: {
+          "content-type": "application/json",
+          'Authorization': 'Bearer my-token',
+      },
+  };
+
+// URL for Create API
+// Fetch API call to the database to create a new entry
+
+fetch(create_fetch, requestOptions)
+    .then(response => {
+      // trap error response from Web API
+      if (response.status !== 200) {
+        const errorMsg = 'Database create error: ' + response.status;
+        console.log(errorMsg);
+        return;
+      }
+      // response contains valid result
+      response.json().then(data => {
+          console.log(data);
+          //add a table row for the new city and airport
+          add_row(data);
+      })
+  })
+}
+
+// Display city-airport table, data is fetched from Backend Database
+function read_entries() {
+  // prepare fetch options
+  const read_options = {
+    method: 'GET',
+    mode: 'cors',
+    cache: 'default',
+    credentials: 'omit',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  };
+  // fetch the data from API
+  fetch(read_fetch, read_options)
+    .then(response => {
+      // check for response errors
+      if (response.status !== 200) {
+          const errorMsg = 'Database read error: ' + response.status;
+          console.log(errorMsg);
+          return;
+      }
+      // valid response will have json data
+      response.json().then(data => {
+          console.log(data);
+          for (let row in data) {
+            console.log(data[row]);
+            add_row(data[row]);
+          }
+      })
+  })
+  // catch fetch errors (ie ACCESS to server blocked)
+  .catch(err => {
+    console.error(err);
+  });
+}
+
+function add_row(data) {
+  const tr = document.createElement("tr");
+  const city1 = document.createElement("td");
+  const airport1 = document.createElement("td");
+  // obtain data that is specific to the API
+  city1.innerHTML = data.city; 
+  airport1.innerHTML = data.airport; 
+
+  // add HTML to container
+  tr.appendChild(city1);
+  tr.appendChild(airport1);
+
+  resultContainer.appendChild(tr);
+}
+
+function delete_entry() {
+  var deleteField = document.getElementById('deleteField').value;
+
+  console.log(deleteField)
+
+  // prepare fetch options
+  const body = {
+      city: deleteField,
+  };
+  const delete_options = {
+    method: 'DELETE',
+    cache: 'default',
+    credentials: 'omit',
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  };
+  // fetch the data from API
+  fetch(delete_fetch, delete_options)
+    .then(response => {
+      // check for response errors
+      // response contains valid result
+      response.json().then(data => {
+          console.log(data);
+      })
+  })
+  // catch fetch errors (ie ACCESS to server blocked)
+  .catch(err => {
+    console.error(err);
+  });
+  // Update the display after entry is deleted
+  read_entries();
+}
+
+</script>
